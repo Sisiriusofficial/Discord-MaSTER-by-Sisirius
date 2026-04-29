faf3ea20661b2d29909106bac69104ce41302bf1
Sisirius <sisiriusx@gmail.com>
2026-04-30 04:50:25 +0600

    Delete src directory

--- src/pages/Logging.tsx
+++
@@ -1,8 +0,0 @@
-export default function Logging() {
-  return (
-    <div>
-      <h1 style={{ fontSize: '1.5em', marginBottom: '16px' }}>📝 Logging</h1>
-      <p style={{ color: 'var(--text-secondary)' }}>Logging configuration coming soon...</p>
-    </div>
-  );
-}
--- src/pages/Tickets.tsx
+++
@@ -1,8 +0,0 @@
-export default function Tickets() {
-  return (
-    <div>
-      <h1 style={{ fontSize: '1.5em', marginBottom: '16px' }}>🎫 Tickets</h1>
-      <p style={{ color: 'var(--text-secondary)' }}>Ticket system configuration coming soon...</p>
-    </div>
-  );
-}
